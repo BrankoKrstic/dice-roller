255da1037bd12d11d753e48244ed431115dabcd8
brankokrstic94 <brankokrstic94@gmail.com>
2026-03-05 19:14:48 +0100

    cleanup

--- src/client/pages/stats.rs
+++ src/client/pages/stats.rs
@@ -12,47 +12,6 @@ stylance::import_style!(style, "stats.module.scss");
 
 use web_sys::{MessageEvent, Worker};
 
-pub struct ChanceCalculatorResult {
-    trials: u32,
-    successes: u32,
-    accumulated_result: i128,
-    success_percent: f64,
-    avg_dmg: f64,
-    avg_dmg_per_attack: f64,
-}
-
-struct ChanceCalculator {
-    trials: u32,
-    successes: u32,
-    accumulated_result: i128,
-}
-
-impl ChanceCalculator {
-    fn new(trials: u32) -> Self {
-        Self {
-            trials,
-            successes: 0,
-            accumulated_result: 0,
-        }
-    }
-
-    fn record_hit(&mut self, result: i64) {
-        self.successes += 1;
-        self.accumulated_result += result as i128;
-    }
-
-    fn finish(self) -> ChanceCalculatorResult {
-        ChanceCalculatorResult {
-            trials: self.trials,
-            successes: self.successes,
-            accumulated_result: self.accumulated_result,
-            success_percent: self.successes as f64 / self.trials as f64,
-            avg_dmg: self.accumulated_result as f64 / self.successes as f64,
-            avg_dmg_per_attack: self.accumulated_result as f64 / self.trials as f64,
-        }
-    }
-}
-
 #[derive(Debug, Clone)]
 enum CalculatorVariant {
     Ac,
